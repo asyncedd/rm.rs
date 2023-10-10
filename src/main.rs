e570919795375d8a36554941132ffad5ec8bdcb1
asyncedd <isynqquwu@proton.me>
2023-10-10 18:09:58 +0900

    fix: Remove unneeded print statements

--- src/main.rs
+++ src/main.rs
@@ -150,7 +150,6 @@ fn main() -> io::Result<()> {
                     )
                     .as_str()))
                     {
-                        println!("OK.");
                         rm!();
                     }
                 }
