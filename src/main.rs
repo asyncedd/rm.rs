17d6771e27618b2d852037068bd7665915a7c7eb
asyncedd <isynqquwu@proton.me>
2023-10-21 17:04:43 +0900

    feat(license): Use GPLv3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+//  A rm replacement written in Rust.
+//  Copyright (C) 2023~ asyncedd<isynqquwu@proton.me>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 mod prelude;
 use prelude::*;
 
@@ -98,7 +114,7 @@ where
     Ok(())
 }
 
-fn main() -> Result<(), Report> {
+fn main() -> Result<()> {
     let opt = Cli::parse();
     color_eyre::install()?;
 
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 pub use clap::Parser;
-pub use color_eyre::{eyre::Report, eyre::WrapErr, Section};
+pub use color_eyre::{eyre::Report, eyre::WrapErr, Result, Section};
 pub use inquire::{Confirm, InquireError};
 pub use std::{
     fs,
